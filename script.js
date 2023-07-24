34805a93b2f79d58bd25a18a989a1d897714bbdf
Koral <koral@Korals-MacBook-Air.local>
2023-07-24 18:10:39 +0300

    index.html and javascript updated

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.getElementById('sinpForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
+function calculate() {
     var education = parseInt(document.getElementById('education').value);
     var experience = parseInt(document.getElementById('experience').value);
     var language = parseInt(document.getElementById('language').value);
     var age = parseInt(document.getElementById('age').value);
     var connection = parseInt(document.getElementById('connection').value);
-
-    var totalPoints = education + experience + language + age + connection;
-    var eligibility = totalPoints >= 60 ? 'Eligible' : 'Not Eligible';
-
-    document.getElementById('result').innerHTML = 'Total Points: ' + totalPoints + ', Eligibility: ' + eligibility;
-});
+    var total = education + experience + language + age + connection;
+    var result = document.getElementById('result');
+    if (total >= 60) {
+        result.textContent = 'Congratulations! Your total points are ' + total + '. You are eligible for the SINP.';
+        result.style.color = 'green';
+    } else {
+        result.textContent = 'Your total points are ' + total + '. Unfortunately, you are not eligible for the SINP.';
+        result.style.color = 'red';
+    }
+}
